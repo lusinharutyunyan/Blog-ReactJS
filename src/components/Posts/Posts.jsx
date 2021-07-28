fd1c7008096a2b378cbe47af1585b83f4ff2b106
Lusin Harutyunian <luusinharutyunian@gmail.com>
2021-07-28 19:47:41 +0400

    feat : editing localStorage items

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./HeaderPosts";
 import YourPost from "./PostPage";
 
 export default function Posts() {
+  const data = localStorage.getItem("post")
+    ? JSON.parse(localStorage.getItem("post"))
+    : [];
+
+  const myPosts = data.map((item) => {
+    return <YourPost data={item} />;
+  });
   return (
     <div>
       <Header />
-      <YourPost />
+      {myPosts}
     </div>
   );
 }
